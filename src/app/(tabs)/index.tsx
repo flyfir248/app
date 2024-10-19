5e640a2668377e3e61c1ad500dd1f65014e504f3
anoop <anoopjohny2000@gmail.com>
2024-10-19 12:52:16 +0530

    Type safe components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { View } from "react-native";
-import products from "../../../assets/data/products";
-import ProductListItem from "../../components/ProductListItem";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      <ProductListItem product={products[7]} />
       <ProductListItem product={products[2]} />
+      <ProductListItem product={products[5]} />
     </View>
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,26 @@
 import { Text, View, StyleSheet, Image } from "react-native";
 import Colors from "../constants/Colors";
+import { Product } from "../types";
 
-const ProductListItem = ({ product }) => {
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  //{product}: ProductListItemProps  >> prevents bugs in production at shipping
+  // Such safe types allow for users and other programmers to prevent and know what type is to be used...
+  //
   //console.log(props);
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{
+          uri:
+            product.image ||
+            "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+        }}
+        style={styles.image}
+      />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
