e65e8109a24831f110877e3d62db6be2ab42e95b
anoop <anoopjohny2000@gmail.com>
2024-10-23 19:09:15 +0530

    Cart menu updates

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -20,6 +20,15 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   const addItem = (product: Product, size: CartItem["size"]) => {
     // if already in cart, increment quantity
 
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
+
     const newCartItem: CartItem = {
       id: randomUUID(), // Id is generated
       product,
@@ -35,11 +44,18 @@ const CartProvider = ({ children }: PropsWithChildren) => {
   //console.log(items);
   const updateQuantity = (itemId: string, amount: -1 | 1) => {
     //console.log(itemId, amount);
-    const updatedItems = items.map((item) =>
-      item.id !== itemId ? item : { ...item, quantity: item.quantity + amount }
+    setItems(
+      items
+        .map((item) =>
+          item.id !== itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
     );
-    setItems(updatedItems);
   };
+
+  //console.log(items);
 
   return (
     <CartContext.Provider value={{ items, addItem, updateQuantity }}>
